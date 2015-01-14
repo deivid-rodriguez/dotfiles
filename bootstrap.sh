aa04f642ee696899dc180e3514ce891abb440302
David Rodríguez <deivid.rodriguez@gmail.com>
2015-01-14 13:19:28 +0100

    Semicolon unnecessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
+git pull origin master
 
 function syncIt() {
-  dirs=`find dotfiles -maxdepth 1 -mindepth 1 -type d`;
+  dirs=`find dotfiles -maxdepth 1 -mindepth 1 -type d`
 
   for d in $dirs
   do
     cp -r $d/. ~/.$(basename $d)
   done
 
-  files=`find dotfiles -maxdepth 1 -mindepth 1 -type f`;
+  files=`find dotfiles -maxdepth 1 -mindepth 1 -type f`
   for f in $files
   do
-    cp -r $f ~/.$(basename $f);
+    cp -r $f ~/.$(basename $f)
   done
 }
 
 function sourceIt() {
-  source ~/.bash_profile;
+  source ~/.bash_profile
 }
 
-syncIt;
-sourceIt;
+syncIt
+sourceIt
 
-unset syncIt;
-unset sourceIt;
+unset syncIt
+unset sourceIt
