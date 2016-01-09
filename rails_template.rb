5f58416ad51b0c4e96867447a587dd224f84bf37
David Rodríguez <deivid.rodriguez@gmail.com>
2016-01-09 14:11:15 +0100

    Add and config overcommit

--- rails_template.rb
+++ rails_template.rb
@@ -15,6 +15,7 @@ gem "rspec-rails"
 gem "rubocop"
 gem "haml-lint"
 gem "scss-lint"
+gem "overcommit"
 
 after_bundle do
   # Rspec initial setup
@@ -22,6 +23,13 @@ after_bundle do
 
   # Configure RuboCop
   file ".rubocop.yml", read_local_dotfile(".rubocop.yml")
+
+  # Configure overcommit
+  file ".overcommit.yml", read_local_dotfile(".overcommit.yml")
+
+  # Install & sign git hooks
+  run "overcommit --install"
+  run "overcommit --sign"
 
   git :init
   git add: "."
