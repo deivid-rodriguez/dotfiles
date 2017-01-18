2065f6b6d2e685f7cb5631548864746e70dcc2e7
David Rodríguez <deivid.rodriguez@gmail.com>
2017-01-18 14:11:26 -0200

    Add a local bin folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ function syncIt() {
   rsync -ah --delete dotfiles/profile.d/ ~/.profile.d
   rsync -ah --delete dotfiles/completion.d/ ~/.completion.d
   rsync -ah dotfiles/config/ ~/.config
+  rsync -ah dotfiles/bin/ ~/.local/bin
 
   files=$(find dotfiles -maxdepth 1 -mindepth 1 -type f)
   for f in $files
