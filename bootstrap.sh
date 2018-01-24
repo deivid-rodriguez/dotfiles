33b76bd9ddd4a59e1404217914fc51bb7695db96
David Rodríguez <deivid.rodriguez@riseup.net>
2018-01-24 15:52:04 -0300

    Make sure prompt script is available

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,10 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
+function download_it {
+  curl -sSL https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > dotfiles/profile.d/prompt
+}
+
 function sync_it {
   rsync -ah --delete dotfiles/profile.d/ ~/.profile.d
   rsync -ah --delete dotfiles/completion.d/ ~/.completion.d
@@ -20,8 +24,10 @@ function source_it {
   source "$HOME/.profile"
 }
 
+download_it
 sync_it
 source_it
 
+unset download_it
 unset sync_it
 unset source_it
